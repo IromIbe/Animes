d5c666545425d414314f41c1971a89428aab4ed8
IromIbe <94483528+IromIbe@users.noreply.github.com>
2022-08-20 15:26:43 +0100

    Revert "added some logic for rendering movies"

--- pages/movies.tsx
+++ pages/movies.tsx
@@ -2,31 +2,11 @@ import * as React from "react";
 import MoviesPage from "./components/moviePg";
 import { GetServerSideProps } from "next";
 import { InferGetServerSidePropsType } from "next";
-import { useState, useEffect } from 'react'
 
 function Movies({
   allMovies,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const query = new URLSearchParams()
-  const [pageNumber, setPageNumber] = useState(query.get('page'))
-  const [movies, setMovies] = useState(allMovies)
-
-  const fetchMovies = async () => {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${api_Key}&language=en-US&page=1`
-    );
-    const data = await res.json();
-
-    setMovies({
-        allMovies: data.results, // data.results is an array of movie objects
-      })
-  }
-
-  useEffect(() => {
-    fetchMovies()
-  }, [pageNumber])
-
-  return <MoviesPage allMovies={movies} />;
+  return <MoviesPage allMovies={allMovies} />;
 }
 
 export default Movies;
